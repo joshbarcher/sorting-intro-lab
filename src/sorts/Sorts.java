42f1449b70155cfc05b2f2fcced74cc75a1b0c2e
AdamZWinter <adam@bootstrapmusic.com>
2023-04-13 10:38:00 -0700

    Insertion sort working

--- src/sorts/Sorts.java
+++ src/sorts/Sorts.java
@@ -21,6 +21,16 @@ public class Sorts
 
     public void insertionSort(int[] array)
     {
+        int size = array.length;
+        for (int i = 1; i < size; i++) {
+            for (int j = i; j >0; j--) {
+                if(array[j]<array[j-1]){
+                    int temp = array[j-1];   //wrote this in class.  So not using swap method from utilities
+                    array[j-1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
 
     }
 }
