967e9846610cf8b1879d00638cf0630a0dee3ec2
AdamZWinter <adam@bootstrapmusic.com>
2023-04-13 10:27:34 -0700

    Selection sort working

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -1,9 +1,20 @@
 package driver;
 
+import sorts.Sorts;
+import utility.SortingUtilities;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        int[] intArray = SortingUtilities.generateRandomArray(20, 0, 50);
+        Sorts sorter = new Sorts();
+        sorter.selectionSort(intArray);
+        if(SortingUtilities.detectAdjacentInversions(intArray)){
+            System.out.println("Detected inversions: this array is NOT sorted.");
+        }else{
+            System.out.println("Detected 0 inversions in the array.");
+        }
 
     }
 }
--- src/sorts/Sorts.java
+++ src/sorts/Sorts.java
@@ -1,11 +1,23 @@
 package sorts;
 
+import utility.SortingUtilities;
+
 public class Sorts
 {
     public void selectionSort(int[] array)
     {
-
+        int indexOfMin;
+        for(int i =0; i < array.length; i++){
+            indexOfMin = i;
+            for(int j = i; j < array.length; j++){
+                if(array[j] < array[indexOfMin]){
+                    indexOfMin = j;
+                }
+            }
+            SortingUtilities.swap(array, i, indexOfMin);
+        }
     }
+
 
     public void insertionSort(int[] array)
     {
